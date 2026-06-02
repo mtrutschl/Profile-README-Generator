f2ddba9c4839bd991c5c6771d0a315bf9ae8d584
BlueFlyingAlien <maya.trutschl@gmail.com>
2026-06-02 18:44:43 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById('generate-button').addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    const subtitle = document.getElementById('subtitle').value;
+    const about = document.getElementById('about').value;
+    const github = document.getElementById('github-user').value;
+    const linkedin = document.getElementById('linkedin-user').value;
+    const instagram = document.getElementById('instagram-user').value;
+
+    let markdown = `# Hi, I'm ${name || 'a Developer'}! 👋\n\n`;
+
+    if (subtitle)
+    {
+        markdown += `### ${subtitle}\n\n`;
+    }
+
+    if (about)
+    {
+        markdown += `## 🚀 About Me 🚀\n${about}\n\n`;
+    }
+
+    if (github || linkedin || instagram)
+});
